181dc625618763ec3e3e6dd34bfb98d38bfe0dc3
KiraYous <1449685718@qq.com>
2022-08-03 01:53:42 +0800

    refactor: use useRef to resolve the problem of which i can not get the latest state of the value of num

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,10 +1,12 @@
-import {useEffect} from 'react'
+import {useEffect,useRef} from 'react'
 import { useDispatch } from 'react-redux/es/hooks/useDispatch';
 import {useState} from 'react'
 import { updateSum } from './counterSlice';
 
 export default function Counter(props){
     const [num, setNum] = useState(0)
+    const numRef=useRef(null)
+    // const numRef={current:num};
     console.log("render",num,"index",props.index)
     const dispatch=useDispatch();
     const increment=()=>{
@@ -16,19 +18,22 @@ export default function Counter(props){
         dispatch(updateSum(-1))
     }
     useEffect(()=>{
+        console.log("setNumRef")
+        numRef.current=num;
+    },[num]);
+    useEffect(()=>{
         // do some requests
         console.log("上一次的num",num,"index",props.index) 
         
         return ()=>{
-            // const action=initAction();
-            let numNow;//缓存上一次的
-            const text=(num)=>{
-                numNow=num;
-                console.log("num",num)
-                // return num
-            }
-            console.log(typeof setNum)
-            setNum(num=>text(num))
+            const numNow=numRef.current;//缓存上一次的
+            // const text=(num)=>{
+            //     numNow=num;
+            //     console.log("num",num)
+            //     // return num
+            // }
+            // console.log(typeof setNum)
+            // setNum(num=>text(num))
             console.log('return 里面的',numNow,"index",props.index);// Todo num值为啥是0,setNum没有更新到useEffect里面的
             dispatch(updateSum(-1*numNow)) //更新redux里面的sum值
         };
